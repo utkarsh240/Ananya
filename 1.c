7858d80b13cdb5aebe7608882f886f1c9147ec4f
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2022-08-30 00:38:50 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,23 +4,30 @@
 
 int main()
 {
-    int n,x;
+    int n, x;
+    int sum = 0;
     printf("Order of Polynomial :- ");
     scanf("%d", &n);
     int *poly = (int *)malloc((n + 1) * sizeof(int));
-    
+
     printf("Enter the coefficients of the polynomials : \n");
     for (int i = 0; i < n + 1; i++)
         scanf("%d", &poly[i]);
 
-    int sum = 0;
-    for (int i = 0; i < n + 1; i++)
-        sum = sum + (poly[i] * pow(x, (n-i)));
 
     printf("The polynomial is \n");
     for (int i = 0; i < n; i++)
-        printf("%d*x^(%d) + ", poly[i], (n-i));
+        printf("%d*x^(%d) + ", poly[i], (n - i));
+
+    printf("The Value of 'x' = ");
+    scanf("%d", &x);
+
     
+    for (int i = 0; i < n + 1; i++)
+        sum = sum + (poly[i] * pow(x, (n - i)));
+
     printf("%d = 0", poly[n]);
+
+    printf("\nThe SUM is %d", sum);
     return 0;
 }
