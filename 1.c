ec3b4b4859fe1298e463a39a6fe0e3d3385d3621
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2022-08-30 00:55:18 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 int main()
 {
-    int n, x;
-    int sum = 0;
-    printf("Order of Polynomial :- ");
+    int n;
+    int x;
+    printf("order of the polynomial : ");
     scanf("%d", &n);
     int *poly = (int *)malloc((n + 1) * sizeof(int));
-
-    printf("Enter the coefficients of the polynomials : \n");
+    printf(" coefficients of the polynomials : \n");
     for (int i = 0; i < n + 1; i++)
+    {
         scanf("%d", &poly[i]);
-
-
-    printf("The polynomial is \n");
-    for (int i = 0; i < n; i++)
-        printf("%d*x^(%d) + ", poly[i], (n - i));
-
-    printf("The Value of 'x' = ");
+    }
+    printf("Enter the value of x \n");
     scanf("%d", &x);
-
-    
+    int sum = 0;
     for (int i = 0; i < n + 1; i++)
+    {
         sum = sum + (poly[i] * pow(x, (n - i)));
-
-    printf("%d = 0", poly[n]);
-
-    printf("\nThe SUM is %d", sum);
+    }
+    printf(" polynomial is \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d*x^(%d) + ", poly[i], (n - i));
+    }
+    printf("%d = 0\n", poly[n]);
+    printf("value after putting value of x is %d .\n", sum);
     return 0;
 }
